8c0e4f27aa12b65d0d59b59988482648fb2f9785
KasiaZd <kasiadzb@onet.eu>
2020-04-06 18:42:10 +0200

    update 06042020

--- Notatnik.java
+++ Notatnik.java
@@ -6,9 +6,12 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 
 class Notatnik extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
 	JFrame ramka; 
 	JMenuBar wstazka;
 	JMenu menu;
@@ -18,6 +21,7 @@ class Notatnik extends JFrame{
 	JMenuItem cofnij, zaznaczWszystko, wklej;
 	JMenu pomoc;
 	JMenu plik;
+	JFileChooser fileChooser;
 
 	
 	Notatnik()
@@ -65,8 +69,82 @@ class Notatnik extends JFrame{
 		
 		ramka.setSize(800, 600);
 		ramka.setVisible(true);
+	}	
+	
+	class OpenListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (JFileChooser.APPROVE_OPTION == fileChooser.showOpenDialog(ramka)) {
+				File plik = fileChooser.getSelectedFile();
+				poleTextowe.setText("");
+				Scanner in = null;
+				try {
+					in = new Scanner(plik);
+					while(in.hasNext()) {
+						String line = in.nextLine();
+						poleTextowe.append(line+"\n");
+						
+					}
+				}
+				catch (Exception ex)
+				{
+					ex.printStackTrace();
+				}
+				finally
+				{
+					in.close();
+				}
+			}
+				
+		}
+	}
+	
+
+	 class SaveListener implements ActionListener{
+		public void actionPerformer(ActionEvent e) {
+			if(JFileChooser.APPROVE_OPTION == fileChooser.showSaveDialog(ramka)) {
+				File plik = fileChooser.getSelectedFile();
+				PrintWriter out = null;
+				
+				try {
+					out = new PrintWriter(plik);
+					String output = poleTextowe.getText();
+					System.out.println(output);
+					out.println(output);
+				}
+				catch(Exception ex) {
+					ex.printStackTrace();
+				}
+				finally {
+					try 
+					{
+						out.flush();
+					}catch(Exception ex1)
+					{
+						
+					}
+					try {
+						out.close();
+					} catch(Exception ex1) {
+				}
+			}
+		}
 	}
 
+}
+		class NewListener implements ActionListener{
+			public void actionPerformed(ActionEvent e) {
+				poleTextowe.setText("");
+			}
+		}
+		
+	class ExitListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+	}
+	
+	
+		
 	public static void main(String [] Args)
 	{
 		Notatnik n = new Notatnik();
