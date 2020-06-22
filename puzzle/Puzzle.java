8343f8b0da4879163bb24a3ba6a72c6b85fcc248
KasiaZd <kasiadzb@onet.eu>
2020-06-22 18:03:21 +0200

    Update Puzzle.java

--- puzzle/Puzzle.java
+++ puzzle/Puzzle.java
@@ -16,16 +16,28 @@ class Puzz extends JFrame implements ActionListener{
 	Icon ikona;
 
 	Icon ikona0 = new ImageIcon("src/konczyna.png");
+	Icon ikona99 = new ImageIcon("src/konczyna.png");
 	Icon przyk  = new ImageIcon("src/glowny1.png");
+	Icon przyk1 = new ImageIcon("src/glowny2");
 	Icon ikona1 = new ImageIcon("src/1.png");
-	Icon ikona2 = new ImageIcon("src/2.png");
-	Icon ikona3 = new ImageIcon("src/3.png");
-	Icon ikona4 = new ImageIcon("src/4.png");
-	Icon ikona5 = new ImageIcon("src/5.png");
+	Icon ikona2 = new ImageIcon("src/5.png");
+	Icon ikona3 = new ImageIcon("src/2.png");
+	Icon ikona4 = new ImageIcon("src/7.png");
+	Icon ikona5 = new ImageIcon("src/4.png");
 	Icon ikona6 = new ImageIcon("src/6.png");
-	Icon ikona7 = new ImageIcon("src/7.png");
-	Icon ikona8 = new ImageIcon("src/8.png");
-	Icon ikona9 = new ImageIcon("src/9.png");
+	Icon ikona7 = new ImageIcon("src/8.png");
+	Icon ikona8 = new ImageIcon("src/9.png");
+	Icon ikona9 = new ImageIcon("src/3.png");
+	
+	Icon ikona11 = new ImageIcon("src/11.png");
+	Icon ikona12 = new ImageIcon("src/12.png");
+	Icon ikona13 = new ImageIcon("src/15.png");
+	Icon ikona14 = new ImageIcon("src/10.png");
+	Icon ikona15 = new ImageIcon("src/13.png");
+	Icon ikona16 = new ImageIcon("src/18.png");
+	Icon ikona17 = new ImageIcon("src/16.png");
+	Icon ikona18 = new ImageIcon("src/14.png");
+	Icon ikona19 = new ImageIcon("src/17.png");
 
 	
 	Puzz(){
@@ -49,7 +61,7 @@ class Puzz extends JFrame implements ActionListener{
 		but9  = new JButton(ikona9);
 		but9.setBounds(210,280,100,100);
 		but10  = new JButton(przyk);
-		but10.setBounds(380,100,200,200);
+		but10.setBounds(380,220,400,400);
 		
 		JLabel labelka = new JLabel("Przykład: ");
 		labelka.setBounds(330,200,70,20);
@@ -116,7 +128,7 @@ class Puzz extends JFrame implements ActionListener{
 				but1.setIcon(i1);
 				but2.setIcon(ikona);
 			}
-			else if(but3.getIcon()== ikona)
+			else if(but5.getIcon()== ikona)
 			{
 				but5.setIcon(i1);
 				but2.setIcon(ikona);
@@ -127,6 +139,41 @@ class Puzz extends JFrame implements ActionListener{
 				but4.setIcon(ikona);
 			}
 				
+		}
+		
+		if(e.getSource() == but3)
+		{
+			Icon i1 = but3.getIcon();
+			if(but2.getIcon() == ikona)
+			{
+				but2.setIcon(i1);
+				but3.setIcon(ikona);
+			}
+			else if(but6.getIcon()== ikona)
+			{
+				but6.setIcon(i1);
+				but3.setIcon(ikona);
+			}
+		}
+		
+		if(e.getSource() == but4)
+		{
+			Icon i1 = but4.getIcon();
+			if(but1.getIcon() == ikona)
+			{
+				but1.setIcon(i1);
+				but4.setIcon(ikona);
+			}
+			else if(but5.getIcon()== ikona)
+			{
+				but5.setIcon(i1);
+				but4.setIcon(ikona);
+			}
+			else if(but7.getIcon()== ikona)
+			{
+				but7.setIcon(i1);
+				but4.setIcon(ikona);
+			}
 		}
 		
 	if(e.getSource() == but5)
@@ -153,6 +200,27 @@ class Puzz extends JFrame implements ActionListener{
 			but5.setIcon(ikona);
 		}
 	}
+	
+	if(e.getSource() == but6)
+	{
+		Icon i1 = but6.getIcon();
+		if(but3.getIcon() == ikona)
+		{
+			but3.setIcon(i1);
+			but6.setIcon(ikona);
+		}
+		else if(but5.getIcon()== ikona)
+		{
+			but5.setIcon(i1);
+			but6.setIcon(ikona);
+		}
+		else if(but9.getIcon()==ikona)
+		{
+			but9.setIcon(i1);
+			but6.setIcon(ikona);
+		}
+	}
+	
 	if(e.getSource() == but7)
 	{
 		Icon i1 = but7.getIcon();
@@ -192,6 +260,11 @@ class Puzz extends JFrame implements ActionListener{
 			but8.setIcon(i1);
 			but9.setIcon(ikona);
 		}
+		else if(but6.getIcon()== ikona)
+		{
+			but6.setIcon(i1);
+			but9.setIcon(ikona);
+		}
 	}
 
 	if(e.getSource() == but10){
@@ -210,6 +283,21 @@ class Puzz extends JFrame implements ActionListener{
 		but9.setIcon(ikona9);
 		ikona = but9.getIcon();
 		but11.setIcon(przyk);
+	}
+	else
+	{
+		but10.setIcon(przyk1);
+		but1.setIcon(ikona11);
+		but2.setIcon(ikona12);
+		but3.setIcon(ikona13);
+		but4.setIcon(ikona14);
+		but5.setIcon(ikona15);
+		but6.setIcon(ikona16);
+		but7.setIcon(ikona17);
+		but8.setIcon(ikona18);
+		but9.setIcon(ikona19);
+		ikona = but9.getIcon();
+		but11.setIcon(przyk1);
 	}
 }
 }
